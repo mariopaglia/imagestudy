525dc208eaf7a266f5fb4e1b42a31244c7cf2448
Mario Paglia <mpagliajr@gmail.com>
2023-01-27 02:01:43 -0300

    Fixed the issue with the global variable for the Pexels API

--- src/api/pexels.tsx
+++ src/api/pexels.tsx
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
 	baseURL: `https://api.pexels.com/v1/`,
 	headers: {
-		Authorization: process.env.PEXELS_API_KEY as string,
+		Authorization: `${process.env.REACT_APP_PEXELS_API_KEY}`,
 	},
 });
